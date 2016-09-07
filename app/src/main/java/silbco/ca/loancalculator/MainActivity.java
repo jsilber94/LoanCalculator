c578f2b7c5bb6be67877b63b11a5b4d5cc4f8fc4
jsilber94 <jessesilber@gmail.com>
2016-09-07 19:02:12 -0400

    final submission

--- app/src/main/java/silbco/ca/loancalculator/MainActivity.java
+++ app/src/main/java/silbco/ca/loancalculator/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -25,9 +26,6 @@ public class MainActivity extends AppCompatActivity {
             int termOfLoan = Integer.parseInt(((EditText) findViewById(R.id.termOfLoan)).getText().toString());
             double yearlyInterestRate = Double.parseDouble(((EditText) findViewById(R.id.yearlyInterestRate)).getText().toString());
 
-            Toast.makeText(MainActivity.this, "Please enter values", Toast.LENGTH_SHORT).show();
-
-
             double monthlyPayment = getMonthlyPayment(termOfLoan, yearlyInterestRate, loanAmount);
             ((TextView) findViewById(R.id.monthlyPayment)).setText(monthlyPayment + "");
 
@@ -36,69 +34,60 @@ public class MainActivity extends AppCompatActivity {
 
             double totalInterest = getTotalInterest(loanAmount, totalLoan);
             ((TextView) findViewById(R.id.totalInterest)).setText(round(totalInterest, 2) + "");
-        }
-        catch(Exception NPE)
-        {
+        } catch (Exception NPE) {
             Toast.makeText(MainActivity.this, "Please enter values", Toast.LENGTH_SHORT).show();
             clear(null);
         }
 
     }
-    public double getTotalInterest(double loanAmount, double totalLoan){
+
+    private double getTotalInterest(double loanAmount, double totalLoan) {
 
         return totalLoan - loanAmount;
     }
-    public double getTotalPayment(int termOfLoan, double monthlyPayment){
 
-
-        return  monthlyPayment * termOfLoan * 12;
-
-
+    private double getTotalPayment(int termOfLoan, double monthlyPayment) {
+        return monthlyPayment * termOfLoan * 12;
     }
 
-    public double getMonthlyPayment(int termOfLoan, double yearlyInterestRate, double loanAmount){
+    private double getMonthlyPayment(int termOfLoan, double yearlyInterestRate, double loanAmount) {
 
         double monthlyPayment;
         double monthlyInterestRate;
         int numberOfPayments;
-        if (termOfLoan != 0 && yearlyInterestRate != 0)
-        {
+        if (termOfLoan != 0 && yearlyInterestRate != 0) {
             //calculate the monthly payment
             monthlyInterestRate = yearlyInterestRate / 1200;
             numberOfPayments = termOfLoan * 12;
 
             monthlyPayment =
                     (loanAmount * monthlyInterestRate) /
-                            (1 - (1 / Math.pow ((1 + monthlyInterestRate), numberOfPayments)));
+                            (1 - (1 / Math.pow((1 + monthlyInterestRate), numberOfPayments)));
 
-            monthlyPayment = Math.round (monthlyPayment * 100) / 100.0;
-        }
-        else
+            monthlyPayment = Math.round(monthlyPayment * 100) / 100.0;
+        } else
             monthlyPayment = 0;
         return monthlyPayment;
 
-
-    }
-    public void clear(View view){
-
-        EditText temp = (EditText)findViewById(R.id.loanAmount);
-        temp.setText("");
-        temp = (EditText)findViewById(R.id.termOfLoan);
-        temp.setText("");
-        temp = (EditText)findViewById(R.id.yearlyInterestRate);
-        temp.setText("");
-
-        TextView temp2 = (TextView)findViewById(R.id.monthlyPayment);
-        temp2.setText("");
-        temp2 = (TextView)findViewById(R.id.totalPayment);
-        temp2.setText("");
-        temp2 = (TextView)findViewById(R.id.totalInterest);
-        temp2.setText("");
     }
 
-    public static double round(double value, int places) {
-        if (places < 0) throw new IllegalArgumentException();
+    public void clear(View view) {
+        EditText temp = (EditText) findViewById(R.id.loanAmount);
+        temp.setText("");
+        temp = (EditText) findViewById(R.id.termOfLoan);
+        temp.setText("");
+        temp = (EditText) findViewById(R.id.yearlyInterestRate);
+        temp.setText("");
 
+        TextView temp2 = (TextView) findViewById(R.id.monthlyPayment);
+        temp2.setText("");
+        temp2 = (TextView) findViewById(R.id.totalPayment);
+        temp2.setText("");
+        temp2 = (TextView) findViewById(R.id.totalInterest);
+        temp2.setText("");
+    }
+
+    private double round(double value, int places) {
         BigDecimal bd = new BigDecimal(value);
         bd = bd.setScale(places, RoundingMode.HALF_UP);
         return bd.doubleValue();
